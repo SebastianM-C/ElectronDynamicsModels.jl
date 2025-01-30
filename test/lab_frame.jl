828bb929b8242216875d4b8b0b41b7e7ca6dad73
Sebastian Micluța-Câmpeanu <sebastian.mc95@proton.me>
2025-01-31 00:30:27 +0200

    test: lab frame representation works now

--- test/lab_frame.jl
+++ test/lab_frame.jl
@@ -20,24 +20,26 @@ sys = structural_simplify(model)
 _γ(v) = 1 / √(1 - (v ⋅ v) / c^2)
 v0 = -c / ((2 / a₀)^2 + 1)
 
-tspan = (0, 50)
+tspan = (0, 350) # t[end] has to be ∫γdτ to be comparable with the proper frame
 
 prob = ODEProblem(
     sys,
     [
-        electron.γ[1] => _γ(v0)
-        electron.v[3] => v0
-        # electron.t => tspan[1]
-        # electron.x => [c * electron.t, 0, 0, 0]
+        electron.γ => _γ(v0)
+        electron.p⃗[1] => 0
         electron.p⃗[2] => 0
-        # electron.p⃗[3] => 0
+        electron.p⃗[3] => _γ(v0)*electron.m*v0
+        electron.τ => 0
+        electron.x[2] => 0
+        electron.x[3] => 0
+        electron.x[4] => 0
     ],
     tspan,
     [],
 )
 
-# sol = solve(prob, Vern9(), abstol = 1e-9, reltol = 1e-9)
-sol = solve(prob, Rodas5P(), abstol = 1e-7)
+sol = solve(prob, Vern9(), abstol = 1e-9, reltol = 1e-9)
+# sol = solve(prob, Rodas5P(), abstol = 1e-7)
 
 plot(sol, idxs = (electron.x[2], electron.x[4]), xlabel = "x", ylabel = "z")
 
@@ -55,21 +57,25 @@ eqs = [
 model = ODESystem(eqs, electron.t; name = :sys, systems = [electron, laser])
 sys = structural_simplify(model)
 
-tspan = (0, 50)
+tspan = (0, 350)
 
 prob = ODEProblem(
     sys,
     [
-        electron.v[1] => 0
-        electron.v[2] => 0
-        electron.γ[1] => _γ(v0)
+        electron.γ => _γ(v0)
+        electron.p⃗[1] => 0
+        electron.p⃗[2] => 0
+        electron.p⃗[3] => _γ(v0) * electron.m * v0
+        electron.τ => 0
+        electron.x[2] => 0
+        electron.x[3] => 0
         electron.x[4] => 0
-        electron.v[3] => v0
     ],
     tspan,
     [],
 )
 
+sol = solve(prob, Vern9(), abstol = 1e-9, reltol = 1e-9)
 sol = solve(prob, Rodas5P(), abstol = 1e-7)
 
 plot(sol, idxs = (electron.x[2], electron.x[4]), xlabel = "x", ylabel = "z")
